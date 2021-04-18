834ac2e10be9cbea9c4b2dfd6bcb12d8819c1641
Alex Waller <alex@artofalexwaller.com>
2021-04-18 18:49:25 -0400

    HybridsForm

---
+++ src/components/HybridsForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class HybridsForm extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default HybridsForm;
