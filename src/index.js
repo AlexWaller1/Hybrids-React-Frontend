b95df451f883c61383d8578796d1419216893b8d
Alex Waller <alex@artofalexwaller.com>
2021-04-18 14:17:24 -0400

    applyMiddleware

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
+import { createStore, applyMiddlware } from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
+
+// need to create a store for redux => 1. reducer 2. dev tools
 
