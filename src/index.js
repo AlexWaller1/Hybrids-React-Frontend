7d59169d59af64d587f24abe0167924d81578b1c
Alex Waller <alex@artofalexwaller.com>
2021-04-18 14:33:21 -0400

    store constant

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore, applyMiddlware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 // need to create a store for redux => 1. reducer 2. dev tools
 
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware))
