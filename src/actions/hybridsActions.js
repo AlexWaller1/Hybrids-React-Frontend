adf3744466da2d766433a2883053f6157c95cb0d
Alex Waller <alex@artofalexwaller.com>
2021-04-18 18:15:25 -0400

    fetchHybrid()

--- src/actions/hybridsActions.js
+++ src/actions/hybridsActions.js
@@ -1,1 +1,7 @@
 // create an action that is going to fetch the grads from api
+
+export const fetchHybrid = () => {
+    return (dispatch) => {
+        fetch('http://localhost:3000/hybrids')
+    }
+}
