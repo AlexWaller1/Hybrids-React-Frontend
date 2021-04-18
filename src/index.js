8e500980ff0a93ef1f7047e73daf0a2a77e27d58
Alex Waller <alex@artofalexwaller.com>
2021-04-18 14:28:51 -0400

    DevTools

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
+// react-redux, redux, thunk
+
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { createStore, applyMiddlware } from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 // need to create a store for redux => 1. reducer 2. dev tools
 
