7e6641a8f37588ff15d75dfbb5c491aad842db35
Alex Waller <alex@artofalexwaller.com>
2021-04-18 14:37:57 -0400

    reactDOM and imports

--- src/index.js
+++ src/index.js
@@ -6,7 +6,16 @@ import { createStore, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension';
-
+import App from './components/App'
 // need to create a store for redux => 1. reducer 2. dev tools
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware))
+const store = createStore(
+    rootReducer, 
+    composeWithDevTools(applyMiddleware(thunk))
+    )
+
+    ReactDOM.render(
+        <Provider store={store}>
+            
+        </Provider>
+    )
