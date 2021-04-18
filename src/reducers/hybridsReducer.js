e03717e2f783c2ec6c47c54c72f4f18d4fc5856e
Alex Waller <alex@artofalexwaller.com>
2021-04-18 15:26:31 -0400

    rootReducer

--- src/reducers/hybridsReducer.js
+++ src/reducers/hybridsReducer.js
@@ -2,5 +2,7 @@ export const hybridsReducer = (state = [], action) => {
      switch(action.type){
          case 'FETCH_HYBRIDS':
              return action.payload
+             default:
+                 return state
      }
 }
