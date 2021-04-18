e9e6d5224aec4a160f6469b4bf9cb7b10e31f919
Alex Waller <alex@artofalexwaller.com>
2021-04-18 19:26:45 -0400

    notes folder

---
+++ notes/componentsNotes.js
@@ -0,0 +1,11 @@
+// components let you split the UI into independent, reusable pieces, 
+// and think about each piece in isolation
+/* Components modularize both functionality and presentation in our code.
+In order to understand how powerful this consider just how intricate web
+applications can become. The difficulty in logically arranging, architecting
+, and programming these web applications increases with their size.
+Components are like little packages: they help us keep everything organized
+and predictable while abstracting the boiler plate code. Each component 
+contains a snippet of code that describes what it should render to
+the DOM 
+*/
