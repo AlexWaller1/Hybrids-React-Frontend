66d4d319ec7c625458ee401fe606f79654fbd113
Alex Waller <alex@artofalexwaller.com>
2021-04-19 11:06:07 -0400

    submit form and <br>

--- src/components/HybridsForm.js
+++ src/components/HybridsForm.js
@@ -24,18 +24,20 @@ class HybridsForm extends Component {
             <form>
                 <label>Hybrid Name:</label>
                 <input type='text' value={this.state.name} onChange={this.handleChange} name="name"/>
-
+                < br />
                 <label>Species Type:</label>
                 <input type='text' value={this.state.species} onChange={this.handleChange} name="species"/>
-
+                < br />
                 <label>Hybrid Personality:</label>
                 <input type='text' value={this.state.personality} onChange={this.handleChange} name="personality"/>
-
+                < br />
                 <label>Hybrid Biography:</label>
                 <input type='text' value={this.state.biography} onChange={this.handleChange} name="biography"/>
-
+                < br />
                 <label>Hybrid Image:</label>
                 <input type='text' value={this.state.image} onChange={this.handleChange} name="image"/>
+                < br />
+                <input type='submit' value="Create Hybrid" />
             </form>
         );
     }
