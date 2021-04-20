d5b53e3adc2da3d63efe20c148b1610018e7e65c
Alex Waller <alex@artofalexwaller.com>
2021-04-20 18:23:26 -0400

    more notes

--- notes/ReactNotes.js
+++ notes/ReactNotes.js
@@ -206,4 +206,199 @@ const element = <div />
 
 However, elements can also represent user-defined components:
 
+const element = <Welcome name="Sara" />;
+
+When React sees an element representing a user-defined component, it passes JSX attributes
+and children to this component as a single object. We call this object "props."
+
+For example, this code renders "Hello, Sara" on the page:
+
+function Welcome(props) {
+    return <h1>Hello, {props.name}</h1>;
+}
+
+const element = <Welcome name="Sara" />;
+ReactDOM.render(
+    element,
+    document.getElementById('root')
+);
+
+Let's recap what happens in this example:
+
+1. We call ReactDOM.render() with the <Welcome name="Bodhi" /> element.
+
+2. React calls the Welcome component with {name: 'Bodhi'} as the props.
+
+Our Welcome component returns a <h1>Hello, Bodhi</h1> element as the result.
+
+React DOM efficiently updates the DOM to match <h1>Hello, Bodhi</h1>.
+
+Note: Always start component names with a capital letter.
+React treats components starting with lowercase letters as DOM tags. For example, <div />
+represents an HTML div tag, but <Welcome /> represents a component and requires Welcome to
+be in scope.
+
+To learn more about the reasoning behind this convention, please read JSX in Depth.
+
+Composing Components
+
+Components can refer to other components in their output. This us use the sam component 
+abstraction for any level of detail. A button, a form, a dialog, a screen: in React
+apps, all those are commonly expressed as components.
+
+For example, we can create an App component that render Welcome many times:
+
+function Welcome(props) {
+    return <h1>Hello, {props.name}</h1>;
+}
+
+function App(){
+    return (
+        <div>
+          <Welcome name="Bodhi" />
+          <Welcome name="Beezer" />
+          <Welcome name="Chipper" />
+        </div>
+    );
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root")
+);
+
+Typically, new react apps have a single App component at the very top. However, if you 
+integrate React into an existing app, you might start bottom-up with a small component
+like Button and gradually work your way to the top of the view hierarchy.
+
+Extracting Components
+
+Don't be afraid to split components into smaller components.
+For example, consider this Comment component:
+
+function Comment(props) {
+    return (
+        <div className="Comment">
+         <div className="UserInfo">
+          <img className="Avatar"
+            src={propsauthor.avatarUrl}
+            alt={props.author.name}
+            />
+          <div className="Comment-text".
+             {props.text}
+          </div>
+          <div className="Comment-date">
+            {formatDate(props.date)}
+          </div>
+        </div>
+    );
+}
+
+It accepts author (an object), text (a string), and date (a date) as props, and describes a 
+comment on a social media website.
+
+This component can be tricky to change because of all the nesting, and it is also hard
+to resue individual parts of it. Let's extract a few components from it.
+
+First, we will extract Avatar:
+
+function Avatar(props) {
+    return (
+        <img className="Avatar"
+          src={props.user.avatarUrl}
+          alt={props.user.name}
+        />
+    );
+}
+
+The Avatar doesn't need to know that it is being rendered inside a Comment. This is why we
+have given its prop a more generic name: user ranther than author.
+
+We recommend naming props from the component's own point of view rather than the context in 
+which it is being used.
+
+We can now simplify Comment a tiny bit.
+
+function Comment(props) {
+  return (
+    <div className="Comment">
+      <div className="UserInfo">
+        <Avatar user={props.author} />
+        <div className="UserInfo-name">
+          {props.author.name}
+        </div>
+      </div>
+      <div className="Comment-text">
+        {props.text}
+      </div>
+      <div className="Comment-date">
+        {formatDate(props.date)}
+      </div>
+    </div>
+  );
+}
+
+Next, we will extract a UserInfo component that renders an Avatar next to the user's
+name:
+
+function UserInfo(props) {
+    return (
+      <div className="UserInfo">
+        <Avatar user={props.user} />
+        <div className="UserInfo-name">
+          {props.user.name}
+        </div>
+      </div>
+    );
+}
+
+This lets us simplify Comment even further:
+
+function Comment(props) {
+    return(
+        <div className="Comment">
+        <UserInfo user={props.author} />
+        <div className="Comment-text">
+          {props.text}
+        </div>
+        <div className="Comment-date">
+           {formatDate(props.date)}
+        </div>
+        <div>
+    );
+}
+
+Extracting components might seem like grunt work at first, but having a palette of
+reusable components pays off in larger apps. A good rule of thumb is that if a part
+of your UI is used several times (Button, Panel, Avatar), or is complex enough on its
+own(App, FeedStory, Comment), it is a good candidate to be extracted to a separate 
+component.
+
+Props are Read-Only
+
+Whether you declare a component as a function of a class, it must never modify its 
+own props. Consider this sum function.
+
+function sum(a, b) {
+    return a + b;
+}
+
+Such functions are called "pure" because they do not attempt to change their inputs,
+and always return the same result for inputs.
+
+In contrast, this function is impure because it changes its own input
+
+function withdraw(account, amount) {
+    account.total -= amount;
+
+    React is pretty flexible but it has a single strict rule:
+
+    All React components must act like pure functions with respect to their props.
+
+    Of course, application UIs are dynamic and change over time. In the next section,
+    we will introduce a new concept of "state." State allows React components to change 
+    their output over time in response to user actions, network responses, and anything 
+    else, without violating this rule.
+}
+
 */
